7e4080ff1d026b137749da057067d60995d7ab5a
abigailmcgovern <abigail_mcgovern@hotmail.com>
2021-11-14 13:44:22 +1100

    Build docs with numpydoc

--- docs/docs/source/conf.py
+++ docs/docs/source/conf.py
@@ -41,7 +41,8 @@ extensions = [
     'sphinx.ext.duration',
     'sphinx.ext.doctest', 
     'sphinx.ext.autodoc', 
-    'sphinx.ext.autosummary', 
+    'sphinx.ext.autosummary',
+    'numpydoc' 
 ]
 
 # Add any paths that contain templates here, relative to this directory.
